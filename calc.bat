347a556d59eba74f99338338901dac4534319fd1
Chiagoziem Mba-Oji <100405629+g0ziem@users.noreply.github.com>
2023-08-20 16:33:44 +0100

    Update calc.bat

--- calc.bat
+++ calc.bat
@@ -1,5 +1,1 @@
-@echo off
-echo Set objShell = CreateObject("WScript.Shell") > %temp%\OpenCalculator.vbs
-echo objShell.Run("calc.exe"), 0 >> %temp%\OpenCalculator.vbs
-cscript //nologo %temp%\OpenCalculator.vbs
-del %temp%\OpenCalculator.vbs
+start calc.exe
